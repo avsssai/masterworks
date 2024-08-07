c502001435221327081cda837087136c31335daf
shiva addanki <avsssai@gmail.com>
2024-08-07 19:04:46 +0530

    artist landing page

--- src/app/data/painters.ts
+++ src/app/data/painters.ts
@@ -10,7 +10,15 @@ interface Painter {
   born: string;
   died: string;
   displayPicture: string;
-  paintings: any;
+  displayPictureAlt: string;
+  paintings: Painting[];
+}
+
+interface Painting {
+  id: number;
+  name: string;
+  year: string;
+  paintingURI: string;
 }
 
 export const paintersData: IProps = {
@@ -21,7 +29,33 @@ export const paintersData: IProps = {
     displayNameSecond: "Brandt",
     born: "MDCVI",
     died: "MDCLXIX",
-    displayPicture: "",
-    paintings: [],
+    displayPicture: "/rembrandt/rembrandt-self.jpg",
+    displayPictureAlt: "Self potrait of the painter Rembrandt.",
+    paintings: [
+      {
+        id: 1,
+        name: "The Night Watch",
+        year: "MDCXLII",
+        paintingURI: "",
+      },
+      {
+        id: 2,
+        name: "The Kitchen Maid",
+        year: "MDCLI",
+        paintingURI: "",
+      },
+      {
+        id: 3,
+        name: "The Artist In Studio",
+        year: "MDCXXVIII",
+        paintingURI: "",
+      },
+      {
+        id: 4,
+        name: "Titus As A Monk",
+        year: "MDCLX",
+        paintingURI: "",
+      },
+    ],
   },
 };
--- src/app/painters/[painter]/page.tsx
+++ src/app/painters/[painter]/page.tsx
@@ -1,4 +1,5 @@
 import { paintersData } from "@/app/data/painters";
+import Image from "next/image";
 
 export default function Page({ params }: { params: { painter: string } }) {
   const {
@@ -8,10 +9,12 @@ export default function Page({ params }: { params: { painter: string } }) {
     born,
     died,
     firstName,
+    displayPicture,
+    displayPictureAlt,
   } = paintersData[params.painter];
   return (
     <section className="h-screen relative">
-      <div className="h-full relative flex flex-col justify-center text-primary uppercase items-center leading-[4rem] md:leading-[10rem] text-center">
+      <div className="h-full relative flex flex-col justify-center text-primary isolate uppercase items-center leading-[4rem] md:leading-[10rem] text-center">
         <h1 className="text-[96px] sm:text-[6rem] md:text-[14rem] lg:text-[16rem] -tracking-[10px] relative">
           <p className="absolute font-primary text-black top-2 -left-12 md:top-10 md:-left-20 text-xs md:text-sm tracking-normal">
             {born}
@@ -21,7 +24,10 @@ export default function Page({ params }: { params: { painter: string } }) {
           </p>
           {displayNameFirst}
         </h1>
-        <h1 className="relative text-[96px] sm:text-[6rem] md:text-[14rem] lg:text-[16rem]  md:-ml-16 -tracking-[10px] before:content-['-']">
+        <h1 className="relative text-[96px] sm:text-[6rem] md:text-[14rem] z-20 lg:text-[16rem]  md:-ml-16 -tracking-[10px] before:content-['-']">
+          <div className="absolute h-[50px] w-[38.5px] md:h-[150px] md:w-[115.5px] right-6 -top-2 md:right-14 md:-top-5 rotate-6 z-0">
+            <Image src={displayPicture} alt={displayPicture} fill />
+          </div>
           {displayNameSecond}
         </h1>
         <h2 className="md:text-[24px] leading-5 mt-4 md:mt-8">{secondName}</h2>
